e5b5ee5cc6a5a5bd2af96154ff4490010fa6f1e4
Derrick <derrickc0527@gmail.com>
2016-09-21 09:16:06 -0400

    Start up project base

---
+++ FaceDetectorStarter/Detector/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Detector
+//
+//  Created by Gregg Mojica on 8/21/16.
+//  Copyright © 2016 Gregg Mojica. All rights reserved.
+//
+
+import UIKit
+import CoreImage
+
+class ViewController: UIViewController {
+    @IBOutlet weak var personPic: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        personPic.image = UIImage(named: "face-1")
+
+    }
+    
+}
